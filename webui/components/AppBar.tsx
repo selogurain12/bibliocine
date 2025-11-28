ffeb7cbdee3f28db15c1f39658531486af65d363
Lora Seguin <lora80123@gmail.com>
2025-11-28 18:15:12 +0100

    feat(bibliocine/webui): add navigation for connexion - #11

--- webui/components/AppBar.tsx
+++ webui/components/AppBar.tsx
@@ -1,8 +1,11 @@
 import { View } from "react-native";
 import { Text } from "./ui/text";
 import { BookOpen, CircleUserRound, Film } from "lucide-react-native";
+import { Button } from "./ui/button";
+import { useNavigation } from "@react-navigation/native";
 
 export function AppBar() {
+    const navigation = useNavigation();
     return (
         <View className="h-16 bg-amber-100 px-4 flex-row items-center gap-2">
             <View className="flex-row justify-start gap-2 w-1/3">
@@ -13,7 +16,9 @@ export function AppBar() {
                 <Text className="text-black text-lg font-bold">Bibliociné</Text>
             </View>
             <View className="flex-row justify-end right-3 w-1/3">
-                <CircleUserRound /> 
+                <Button className="flex-col items-center gap-1" variant="ghost" onPress={() => navigation.navigate('Profile')}>
+                    <CircleUserRound /> 
+                </Button>
             </View>
         </View>
     )
