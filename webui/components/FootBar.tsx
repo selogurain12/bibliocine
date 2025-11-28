a8f8a2e9570338f8d8b1a4b2507e9dcd59da6459
Lora Seguin <lora80123@gmail.com>
2025-11-28 18:22:47 +0100

    feat(bibliocine/webui): add navigation in footbar

--- webui/components/FootBar.tsx
+++ webui/components/FootBar.tsx
@@ -2,9 +2,10 @@ import { View } from 'react-native';
 import { BookOpen, Film, Library } from 'lucide-react-native';
 import { Button } from './ui/button';
 import { Text } from './ui/text';
-import { Separator } from './ui/separator';
+import { useNavigation } from '@react-navigation/native';
 
 export function FootBar() {
+  const navigation = useNavigation();
   return (
     <View className="h-16 flex-row bg-amber-100 justify-center items-center border-t border-gray-200">
       <View className="w-1/3 flex-row justify-center">
@@ -14,7 +15,7 @@ export function FootBar() {
         </Button>
       </View>
       <View className="w-1/3 flex-row justify-center">
-        <Button className="flex-col items-center gap-1" variant="ghost">
+        <Button className="flex-col items-center gap-1" variant="ghost" onPress={() => navigation.navigate('Book')}>
           <BookOpen size={20} />
           <Text className="text-xs">Livres</Text>
         </Button>
