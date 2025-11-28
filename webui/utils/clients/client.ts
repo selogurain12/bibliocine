06533c5f674d0e18f1dd31c57bb3fd6a78ffb1bd
Lora Seguin <lora80123@gmail.com>
2025-11-28 18:17:50 +0100

    feat(biblioxine/webui): update client - #7

--- webui/utils/clients/client.ts
+++ webui/utils/clients/client.ts
@@ -1,30 +1,48 @@
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import {
+  type ApiFetcherArgs,
+  type AppRouter,
+  tsRestFetchApi,
+} from "@ts-rest/core";
 import { localStorageBasePrefixVariable } from "../local-storage-base-prefix-variable";
 import { biblioCineContract } from "../../../packages/src/contracts/index.contract";
 
-const apiUrl = "http://10.0.2.1:3000";
-console.log("API URL:", apiUrl);
+const apiUrl = "https://bibliocine.onrender.com";
 
+// Récupération du token depuis AsyncStorage
 async function getIdTokenAsync(): Promise<string | null> {
   try {
-    const raw = await AsyncStorage.getItem(localStorageBasePrefixVariable("idToken"));
-    return raw;
+    return await AsyncStorage.getItem(localStorageBasePrefixVariable("idToken"));
   } catch (error) {
     console.error("Erreur lors de la récupération du token :", error);
     return null;
   }
 }
-export const clientPromise = initTsrReactQuery(
-  biblioCineContract,
-  {
-    baseUrl: apiUrl,
-    headers: {
-      "x-app-source": "ts-rest",
-      "x-access-token": async () => await getIdTokenAsync(),
-    },
-  }
-);
 
-export const client = clientPromise;
+export function createClient<Tcontract extends AppRouter>(contract: Tcontract) {
+  return initTsrReactQuery(contract, {
+    baseUrl: apiUrl,
+
+    baseHeaders: {
+      "Content-Type": "application/json",
+    },
+
+    jsonQuery: false,
+
+    api: async (args: ApiFetcherArgs) => {
+      const idToken = await getIdTokenAsync();
+      const headers: Record<string, string> = {
+        "Content-Type": "application/json",
+      };
+      if (idToken) {
+        console.log("Using id token:", idToken);
+        headers.Authorization = `Bearer ${idToken}`;
+      }
+      return await tsRestFetchApi({ ...args, headers });
+    },
+  });
+}
+
+
+export const client = createClient(biblioCineContract);
