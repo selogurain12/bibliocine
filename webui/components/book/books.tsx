dddef40a1ce31689458b86b882b2f97681fc6703
Lora Seguin <lora80123@gmail.com>
2025-12-06 14:11:32 +0100

    feat(bibliocine/webui): list books with search - #10

---
+++ webui/components/book/books.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { View, ActivityIndicator, FlatList, Image, TouchableOpacity } from "react-native";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Text } from "../ui/text";
+import { queryKeys } from "../../../packages/src/query-client";
+import { client } from "../../utils/clients/client";
+import { BookDto } from "../../../packages/src/dtos/book.dto";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "App";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+type NavProp = NativeStackNavigationProp<RootStackParamList, "Book">;
+
+export function Books() {
+  const [search, setSearch] = useState("");
+  const navigation = useNavigation<NavProp>();
+  const insets = useSafeAreaInsets();
+
+  const { data, isLoading, refetch } = client.books.getAllBooks.useQuery({
+    queryKey: queryKeys.books.getAllBooks({
+      pathParams: { search },
+    }),
+    queryData: {
+      params: { search },
+    },
+    enabled: false,
+  });
+
+  const renderItem = ({ item }: { item: BookDto }) => {
+    const imageUri = item.imageLink
+      ? item.imageLink
+      : "https://via.placeholder.com/100x150?text=No+Image";
+
+    return (
+      <TouchableOpacity
+        className="flex-1 m-2"
+        onPress={() => navigation.navigate("BookDetail", { id: item.id })}
+      >
+        <View className="flex-1 m-2">
+          <View className="flex-1 bg-white rounded-lg shadow items-center p-2 border border-gray-200">
+            <Image
+              source={{ uri: imageUri }}
+              className="w-full aspect-[2/3] rounded-md"
+              resizeMode="cover"
+            />
+            <Text className="mt-2 text-center" numberOfLines={2}>{item.title}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View className="p-2">
+      <View className="flex-row items-center gap-2 mb-4">
+        <Input
+          className="w-4/6"
+          placeholder="Titre du livre"
+          value={search}
+          onChangeText={setSearch}
+        />
+        <Button
+          variant="outline"
+          className="w-2/6 right-1"
+          onPress={() => {
+            refetch();
+          }}
+        >
+          <Text>Chercher</Text>
+        </Button>
+      </View>
+
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          contentContainerStyle={{ paddingBottom: insets.bottom + 5 }}
+          data={data?.body.data}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={2}
+        />
+      )}
+    </View>
+  );
+}
