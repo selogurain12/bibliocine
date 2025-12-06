552a0d7e1e68d15e5b71c036048a30880ec10b6e
Lora Seguin <lora80123@gmail.com>
2025-12-06 14:10:14 +0100

    feat(bibliocine/webui): create and update bibliotheque modal - #15

---
+++ webui/components/bibliotheque/forms/update.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { Modal, View, TouchableOpacity } from "react-native";
+import { Text } from "../../ui/text";
+import { Label } from "../../ui/label";
+import { Input } from "../../ui/input";
+import { Controller, useForm } from "react-hook-form";
+import { BibliothequeDto, UpdateBibliothequeDto, updateBibliothequeSchema } from "../../../../packages/src/dtos/bibliotheque.dto";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { client } from "utils/clients/client";
+import { queryClient } from "context/query-client";
+import { queryKeys } from "../../../../packages/src/query-client";
+import { useToast } from "../../ui/toast";
+import { useAuth } from "context/auth-context";
+import { isFetchError } from "@ts-rest/react-query/v5";
+
+type UpdateBibliothequeProps = {
+  bibliotheque: BibliothequeDto;
+  visible: boolean;
+  onClose: () => void;
+};
+
+export function UpdateBibliotheque({ bibliotheque, visible, onClose }: UpdateBibliothequeProps) {
+  const { showToast } = useToast();
+  const { user } = useAuth();
+    
+  const form = useForm<UpdateBibliothequeDto>({
+    resolver: zodResolver(updateBibliothequeSchema),
+    defaultValues: { name: bibliotheque.name },
+  });
+
+  if (!user) {
+      showToast("Vous devez être connecté pour modifier une bibliotheque", 2000, "error");
+      return;
+    }
+
+   const { mutate } = client.bibliotheque.updateBibliotheque.useMutation({
+      onSuccess: ({body}) => {
+        void queryClient.invalidateQueries({
+          queryKey: queryKeys.bibliotheque.getAllBibliotheques({
+            pathParams: { userId: user.id },
+          }),
+        });
+        form.reset();
+        showToast("La bibliotheque a bien été modifiée !", 2000, "success");
+        onClose();
+      },
+      onError: (error) => {
+        if (isFetchError(error)) {
+          showToast(`Erreur lors de la modification : ${error.message}`, 4000, "error");
+        }
+      },
+    });
+
+  function handleSubmit(data: UpdateBibliothequeDto) {
+    if (!user) {
+      showToast("Vous devez être connecté pour modifier une bibliothèque", 2000, "error");
+      return;
+    }
+    mutate({
+      params: { id: bibliotheque.id, userId: user.id },
+      body: {
+        name: data.name,
+        books: bibliotheque.books
+      },
+    });
+  }
+
+  return (
+    <Modal visible={visible} transparent animationType="fade" onRequestClose={onClose}>
+      <View className="flex-1 bg-black/50 justify-center items-center">
+        <View className="bg-white p-6 rounded-lg w-3/4">
+          <Text className="text-lg font-bold mb-4">Modifier une bibliotheque</Text>
+
+          <View className="gap-1.5 mb-4">
+            <Label htmlFor="name">Nom de la bibliotheque</Label>
+            <Controller
+              control={form.control}
+              name="name"
+              rules={{ required: "Le nom est obligatoire" }}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  id="name"
+                  placeholder="Ex: Mes livres préférés"
+                  value={value}
+                  onChangeText={onChange}
+                  returnKeyType="done"
+                />
+              )}
+            />
+            {form.formState.errors.name && (
+              <Text className="text-red-500">{form.formState.errors.name.message}</Text>
+            )}
+          </View>
+
+          <View className="flex-row justify-between">
+            <TouchableOpacity onPress={onClose} className="bg-gray-400 p-3 rounded-md flex-1 mr-2">
+              <Text className="text-white text-center">Fermer</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={form.handleSubmit(handleSubmit)}
+              className="bg-blue-600 p-3 rounded-md flex-1 ml-2"
+            >
+              <Text className="text-white text-center">Modifier</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
